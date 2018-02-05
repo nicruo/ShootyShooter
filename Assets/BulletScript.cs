ea1ebf67adf55fa7280c69b249f7386a90cc73eb
José Medeiros <jmedeiros@agap2.pt>
2018-02-05 19:11:55 +0000

    First commit;

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * 1000);
+        Destroy(gameObject, 2);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyScript>().DamageTaken(50);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour {
+
+    private Rigidbody body;
+    public float movementSpeed = 10;
+
+    public float maxHealth = 100;
+    public float currentHealth = 100;
+
+    public Text healthText;
+
+    public void DamageTaken(float damage)
+    {
+        currentHealth -= damage;
+        healthText.text = currentHealth + "";
+    }
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+
+        healthText = transform.GetComponentInChildren<Text>();
+
+        healthText.text = currentHealth + "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(currentHealth <= 0)
+        {
+            DestroyImmediate(gameObject);
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        body.AddForce(transform.forward * movementSpeed);
+    }
+}
